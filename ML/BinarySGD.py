2107e3df9fbecaabba6b82b4d6b33288d1cb937b
Thunder <nonthacha.h@gmail.com>
2024-07-11 15:31:06 +0700

    Binary sgd classifier

--- ML/BinarySGD.py
+++ ML/BinarySGD.py
@@ -3,6 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_predict
+import itertools
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+
+def displayConfusionMatrix(cm,cmap=plt.cm.GnBu):
+    classes=["Other Number","Number 5"]
+    plt.imshow(cm,interpolation='nearest',cmap=cmap)
+    plt.title("Confusion Matrix")
+    plt.colorbar()
+    trick_marks=np.arange(len(classes))
+    plt.xticks(trick_marks,classes)
+    plt.yticks(trick_marks,classes)
+    thresh=cm.max()/2
+    for i , j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+        plt.text(j,i,format(cm[i,j],'d'),
+        horizontalalignment='center',
+        color='white' if cm[i,j]>thresh else 'black')
+
+    plt.tight_layout()
+    plt.ylabel('Actually')
+    plt.xlabel('Prediction')
+    plt.show()
 
 def displayimage(x):
     plt.imshow(x.reshape(28,28),
@@ -21,24 +45,24 @@ mnist = {
 }
 x,y = mnist["data"],mnist["target"]
 
-print(mnist["data"].shape)
-print(mnist["target"].shape)
-
 # training set
 # class 0-9
 x_train, x_test ,y_train, y_test = x[:60000],x[60000:],y[:60000],y[60000:]
 
 # class 0, ไม่ใช่ class 0
 # ข้อมูล 5000 ค่า -> model -> class 0 หรือไม่ ? true : false
-predict_number = 1000
-y_train_0 = (y_train==0)
-y_test_0 = (y_test==0)
+predict_number = 5500
+y_train_5 = (y_train==5)
+y_test_5 = (y_test==5)
 
 sgd_clf = SGDClassifier()
-sgd_clf.fit(x_train,y_train_0)
+sgd_clf.fit(x_train,y_train_5)
 
-score = cross_val_score(sgd_clf,x_train,y_train_0,cv=5,scoring="accuracy")
-print(score)
+y_train_predict = cross_val_predict(sgd_clf,x_train,y_train_5,cv=3)
+cm = confusion_matrix(y_train_5,y_train_predict)
 
-displayPredict(sgd_clf,y_test_0[predict_number],x_test[predict_number])
-displayimage(x_test[predict_number])
+y_test_predict = sgd_clf.predict(x_test)
+
+classes = ['Other Number', 'Number 5']
+print(classification_report(y_test_5,y_test_predict,target_names=classes))
+print("accuracy score",accuracy_score(y_test_5,y_test_predict)*100)
